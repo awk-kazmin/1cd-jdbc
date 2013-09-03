dd1284a7b21aa1182b1af44c2756b47968465498
awk795@gmail.com <awk795@gmail.com@f519337d-4a72-aa5d-cfde-5d7d3c817c48>
2013-09-03 07:54:40 +0000

    Extract superclass Object1C for field, index and table.

--- trunk/src/main/java/ru/spb/awk/driver/for1c/been/Index1C.java
+++ trunk/src/main/java/ru/spb/awk/driver/for1c/been/Index1C.java
@@ -8,15 +8,14 @@ package ru.spb.awk.driver.for1c.been;
  *
  * @author Василий Казьмин
  */
-public class Index1C {
+public class Index1C extends Object1C {
     private int lenght;
-    private String name;
     private Field1C field;
     private Table1C source;
     private boolean unique;
 
-    public Index1C(String string, int i, Field1C field) {
-        name = string;
+    public Index1C(String name, int i, Field1C field) {
+        setName(name);
         lenght = i;
         this.field = field;
     }
@@ -24,14 +23,7 @@ public class Index1C {
     public Index1C(Table1C table) {
         source = table;
     }
-    
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String image) {
-        name = image;
-    }
 
     public void setUnique(boolean equals) {
         unique = equals;
